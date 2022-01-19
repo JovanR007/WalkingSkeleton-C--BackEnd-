092a490f7d6f219c79c0fcbd314a15a4d7d8ba6c
JovanR007 <shearjovan7@gmail.com>
2022-01-19 08:06:28 +0800

    first commit

---
+++ API/Entities/studentUser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class studentUser
+    {
+        public int Id {get; set;}
+        public string? studentName { get;set;} 
+        public string? yearLevel {get;set;}
+    }
+}
